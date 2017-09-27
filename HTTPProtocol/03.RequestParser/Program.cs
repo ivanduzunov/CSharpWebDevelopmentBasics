c77685977fb6cdce6529659559d69c16e6297cbf
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-09-27 10:15:42 +0300

    t

---
+++ HTTPProtocol/03.RequestParser/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.RequestParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, HashSet<string>> dict
+                = new Dictionary<string, HashSet<string>>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                var tokens = input.Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+
+                var path = tokens[0] + " HTTP";
+                var method = tokens[1];
+
+                if (!dict.ContainsKey(method))
+                {
+                    dict.Add(method, new HashSet<string>());
+                }
+
+                dict[method].Add(path);
+            }
+
+            var request = Console.ReadLine().Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var requestedMethod = request[0].ToLower().Trim();
+            var requestedPath = request[1];
+
+            bool isCorrect 
+                = dict.ContainsKey(requestedMethod) && dict[requestedMethod].Contains(requestedPath);
+
+            if (isCorrect)
+            {
+                Console.WriteLine
+                    ("HTTP/1.1 200 OK\nContent-Length: 2\nContent-Type: text/plain\n\nOK");
+            }
+            else
+            {
+                Console.WriteLine
+                    ("HTTP/1.1 404 NotFound\nContent-Length: 9\nContent-Type: text/plain\n\nNotFound");
+            }
+        }
+    }
+}
