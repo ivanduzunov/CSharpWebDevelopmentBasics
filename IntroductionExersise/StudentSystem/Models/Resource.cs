eddf7fe640d46ff0de9de8bea862084f16b9b209
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-09-28 09:31:31 +0300

    daa

--- IntroductionExersise/StudentSystem/Models/Resource.cs
+++ IntroductionExersise/StudentSystem/Models/Resource.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
-using StudentSystem.Validations;
 
 namespace StudentSystem.Models
 {
--- IntroductionExersise/StudentSystem/Program.cs
+++ IntroductionExersise/StudentSystem/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Extensions.Internal;
+using StudentSystem.Models;
 
 namespace StudentSystem
 {
@@ -7,8 +11,65 @@ namespace StudentSystem
         static void Main(string[] args)
         {
             StudentSystemContext db = new StudentSystemContext();
-            db.Database.EnsureDeleted(); 
-            db.Database.EnsureCreated();
+            //db.Database.EnsureDeleted(); 
+            //db.Database.EnsureCreated();
+
+            StudentsAndHomeworks(db);
+            CoursesAndResourses(db);
+            CoursesWithMoreThanFiveResourses(db);
+
+
+
+
+        }
+
+        private static void CoursesWithMoreThanFiveResourses(StudentSystemContext db)
+        {
+            var result = db.Courses
+                .Select(c => new {c.Name, c.StartDate, ResourseCount = c.Resources.Count})
+                .OrderByDescending(c => c.ResourseCount)
+                .ThenByDescending(c => c.StartDate);
+
+            foreach (var course in result)
+            {
+                Console.WriteLine($"{course.Name} --- {course.ResourseCount}");
+            }
+        }
+
+        private static void CoursesAndResourses(StudentSystemContext db)
+        {
+            var courses = db.Courses.Select(c => new {c.Name, c.Description, c.Resources,});
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Name);
+                Console.WriteLine(course.Description);
+
+                foreach (var resourse in course.Resources)
+                {
+                    Console.WriteLine($"{resourse.Name} {resourse.ResourceType} {resourse.URL}");
+                }
+
+
+            }
+        }
+
+        private static void StudentsAndHomeworks(StudentSystemContext db)
+        {
+            var result = db.Students.Select(h => new
+            {
+                h.Name,
+                Homework = h.Homeworks.Select(s => new { s.Content, s.ContentType })
+            });
+
+            foreach (var student in result)
+            {
+                Console.WriteLine(student.Name);
+                foreach (var homework in student.Homework)
+                {
+                    Console.WriteLine($"Homework name: {homework.Content}, Type: {homework.ContentType}");
+                }
+            }
         }
     }
 }
