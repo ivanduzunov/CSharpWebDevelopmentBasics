aaebdc8bba0c34a872cb5599fa39229dd77e74d7
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-09-29 15:31:53 +0300

    dsdsdsds

---
+++ AsynchronousProgramming/01.EvenNumbersTread/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace _01.EvenNumbersTread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var tread = new Thread(() => PrintEvenNumbersInRange(input[0], input[1]));
+
+            tread.Start();
+            tread.Join();
+
+            Console.WriteLine("Finished");
+        }
+
+        private static void PrintEvenNumbersInRange(int v1, int v2)
+        {
+            for (int i = v1; i < v2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ AsynchronousProgramming/02.SliceFile/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _02.SliceFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string videoPath = Console.ReadLine();
+            string destination = Console.ReadLine();
+            int pieces = int.Parse(Console.ReadLine());
+
+            SliceAsync(videoPath, destination, pieces);
+
+            Console.WriteLine("Anything else?");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "No")
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void SliceAsync(string videoPath, string destination, int pieces)
+        {
+            Task.Run(() => Slice(videoPath, destination, pieces));
+        }
+
+        static void Slice(string sourceFile, string destinationPath, int parts)
+        {
+
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
+            using (var source = new FileStream(sourceFile, FileMode.Open))
+            {
+                FileInfo fileInfo = new FileInfo(sourceFile);
+                long partLenght = (source.Length / parts) + 1;
+                long currentByte = 0;
+
+                for (int i = 1; i <= parts; i++)
+                {
+                    string filePath =
+                        string.Format($"{destinationPath}/Part-{i}{fileInfo.Extension}");
+
+                    using (var dest = new FileStream(filePath, FileMode.Create))
+                    {
+                        //new byte[BufferLenght];
+                        byte[] buffer = new byte[filePath.Length];
+
+                        while (currentByte <= partLenght * i)
+                        {
+                            int readBytesCount = source.Read(buffer, 0, buffer.Length);
+                            if (readBytesCount == 0)
+                            {
+                                break;
+                            }
+                            dest.Write(buffer, 0, readBytesCount);
+                            currentByte += readBytesCount;
+                        }
+
+                        Console.WriteLine("Slice Complete.");
+                    }
+                }
+            }
+        }     
+    }
+}
