49a5c32aacaba092cc2a83a4eb87efb21fa08f51
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-10-05 19:56:36 +0300

    rtryr

---
+++ HandmadeHTTPServer/WebServer/Server/HTTP/Response/HttpResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using WebServer.Server.HTTP.Contracts;
+
+namespace WebServer.Server.HTTP.Response
+{
+    public abstract class HttpResponse : IHttpResponse
+    {
+        private readonly IView view;
+
+        protected HttpResponse(string redirectUrl)
+        {
+            this.HeaderCollection = new HttpHeaderCollection();
+            this.StatusCode = HttpStatusCode.Found;
+            this.AddHeader("Location", redirectUrl);
+        }
+
+        protected HttpResponse(HttpStatusCode responseCode, IView view)
+        {
+            this.HeaderCollection = new HttpHeaderCollection();
+            //todo
+            //todo
+        }
+
+        private HttpHeaderCollection HeaderCollection { get; set; }
+
+        private HttpStatusCode StatusCode { get; set; }
+
+        private string StatusMessage => this.StatusCode.ToString();
+
+        private void AddHeader(string location, string redirectUrl)
+        {
+            //todo
+        }
+    }
+}
