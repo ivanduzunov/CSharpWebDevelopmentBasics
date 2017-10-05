885fc23972033a69eac9f6e9b26476149361e962
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-10-05 18:11:34 +0300

    tertertertr

---
+++ HandmadeHTTPServer/WebServer/Server/HTTP/Contracts/IHttpResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServer.Server.HTTP.Contracts
+{
+    public interface IHttpResponse
+    {
+    }
+}
---
+++ HandmadeHTTPServer/WebServer/Server/HTTP/HttpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebServer.Server.HTTP.Contracts;
+
+namespace WebServer.Server.HTTP
+{
+    public class HttpContext : IHttpContext
+    {
+        private readonly IHttpRequest request;
+
+        public HttpContext(string requestString)
+        {
+            this.request = new HttpRequest(requestString);
+        }
+
+        public IHttpRequest Request => this.request;
+    }
+}
