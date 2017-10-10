acaff361329ed60a4e14514b783b2a07ea25a86b
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-10-10 16:59:58 +0300

    Kenovs webServer and HttpProtocol Exersise - part

---
+++ MyCoolWebServer-master/MyCoolWebServer/ByTheCakeApplication/ByTheCakeApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyCoolWebServer.ByTheCakeApplication.Controllers;
+using MyCoolWebServer.Server.Contracts;
+using MyCoolWebServer.Server.Routing.Contracts;
+
+namespace MyCoolWebServer.ByTheCakeApplication
+{
+    public class ByTheCakeApp : IApplication
+    {
+        public void Configure(IAppRouteConfig appRouteConfig)
+        {
+            appRouteConfig.Get
+                ("/", req => new HomeController().Index());
+
+            appRouteConfig.Get
+                ("/about", req => new HomeController().About());
+        }
+    }
+}
---
+++ MyCoolWebServer-master/MyCoolWebServer/ByTheCakeApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MyCoolWebServer.ByTheCakeApplication.Views;
+using MyCoolWebServer.Server.Enums;
+using MyCoolWebServer.Server.Http.Contracts;
+using MyCoolWebServer.Server.Http.Response;
+
+namespace MyCoolWebServer.ByTheCakeApplication.Controllers
+{
+    public class HomeController
+    {
+        public IHttpResponse Index()
+        {
+            var indexHtml = File.ReadAllText(@"ByTheCakeApplication\Resourses\index.html");
+
+            return new ViewResponse
+                (HttpStatusCode.Ok, new IndexView(indexHtml));
+        }
+
+        public IHttpResponse About()
+        {
+            var indexHtml = File.ReadAllText(@"ByTheCakeApplication\Resourses\about.html");
+
+            return new ViewResponse
+                (HttpStatusCode.Ok, new IndexView(indexHtml));
+        }
+
+
+    }
+}
