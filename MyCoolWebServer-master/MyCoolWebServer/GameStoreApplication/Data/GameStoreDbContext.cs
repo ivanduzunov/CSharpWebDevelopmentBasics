7458dafc73afe02a3bfe616d7ed86d30a242b2f6
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-10-13 15:31:50 +0300

    rerere

---
+++ MyCoolWebServer-master/MyCoolWebServer/GameStoreApplication/Data/GameStoreDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyCoolWebServer.GameStoreApplication.Data
+{
+    public class GameStoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)/.;Database=GameStoreDB;Integrated Security=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+
+    }
+}
---
+++ MyCoolWebServer-master/MyCoolWebServer/GameStoreApplication/GameStoreApp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCoolWebServer.GameStoreApplication
+{
+    class GameStoreApp
+    {
+    }
+}
